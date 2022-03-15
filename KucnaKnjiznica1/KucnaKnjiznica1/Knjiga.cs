39584eb27436ef68f4ca10aa86dddffb731981b9
MartaMarija <mpicic@foi.hr>
2022-03-15 10:23:31 +0100

    Zadatak

---
+++ KucnaKnjiznica1/KucnaKnjiznica1/Knjiga.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KucnaKnjiznica1
+{
+    internal class Knjiga
+    {
+        public string ISBN { get; set; }
+        public string Naslov { get; set; }
+        public enum StatusKnjige 
+        {
+            Dostupno, NaPosudbi
+        }
+
+        public StatusKnjige Status = new StatusKnjige();
+
+        public Knjiga(string ISBN, string Naslov, StatusKnjige status)
+        {
+            this.Status = status;
+            this.ISBN = ISBN;
+            this.Naslov = Naslov;
+            
+        }
+    }
+}
---
+++ KucnaKnjiznica1/KucnaKnjiznica1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KucnaKnjiznica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Unesite ISBN knjige koju posudujete:");
+                string isbn = Console.ReadLine();
+
+                Knjiznica k = new Knjiznica();
+
+                k.Posudi(isbn);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
